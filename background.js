dfd62aef1135c9c715077b6b0b2f12d03844e9d5
Theadejong <theadejong85@gmail.com>
2021-08-23 16:35:33 +0200

    background

---
+++ background.js
@@ -0,0 +1,16 @@
+const bgImg = document.createElement('img');
+bgImg.src = './Images/Background.png';
+class Background {
+    constructor( canvasContext ){
+        (this.ctx = canvasContext),
+        (this.x = 0),
+        (this.y = 0),
+        (this.width = 800),
+        (this.height = 800);
+    }
+
+    draw(){
+        this.ctx.drawImage(bgImg, this.x, this.y, this.width, this.height);
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+let canvas = document.querySelector('#myCanvas');
+let ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 800;
+//console.log("Check if JS is connected")
+//console.log(myCanvas)
